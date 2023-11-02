e5e9290c9ef238f849e6fdfa5039383a566a2cd9
BlueDragn <76735748+BlueDragn@users.noreply.github.com>
2023-11-02 15:04:07 +0530

    added script

---
+++ src/script.js
@@ -0,0 +1,31 @@
+
+
+
+
+function myFunction1(){
+    const marker1 = document.getElementById("x");
+    const marker1Value = marker1.value;
+    const playerOne = {
+        name:"p1",
+        marker:marker1Value,
+    }
+    alert("player1: " + playerOne.marker);
+    return playerOne.marker;
+
+};
+
+
+function myFunction2(){
+const marker2 = document.getElementById("o");
+const marker2Value = marker2.value;
+
+    const playerOne = {
+        name:"p1",
+        marker:marker2Value,
+    }
+    alert("player1: " + playerOne.marker);
+    return playerOne.marker;
+
+};
+
+
